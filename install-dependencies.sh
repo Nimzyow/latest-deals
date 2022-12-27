1c237c93300e3d09c102974e96ce783453eca5e7
Nima Golzar Soufiani <nima.soufiani@rippleenergy.com>
2022-12-27 13:20:29 +0000

    add layer

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+Green='\033[0;32m'
+Color_Off='\033[0m' 
+
 for directory in ./src/functions/* ; do (cd "$directory" && yarn install && echo "insalled packages for $directory"); done
-echo "All function packages have been installed"
+echo "${Green}All function packages have been installed"
+
+for directory in ./src/layers/* ; do (cd "$directory" && yarn install && echo "insalled packages for $directory"); done
+echo "${Green}All layer packages have been installed"
